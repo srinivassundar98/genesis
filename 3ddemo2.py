d89bc2a9bf7d1621a7007b05c4460ef913d84e31
srinivassundar98 <39563155+srinivassundar98@users.noreply.github.com>
2018-07-14 22:23:35 +0530

    Add files via upload

---
+++ 3ddemo2.py
@@ -0,0 +1,66 @@
+
+import numpy as np
+import matplotlib.pyplot as plt
+# Though the following import is not directly being used, it is required
+# for 3D projection to work
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import style
+style.use('ggplot')
+from sklearn.cluster import KMeans
+from sklearn import datasets
+
+
+iris = datasets.load_iris()
+X = iris.data
+y = iris.target
+
+estimators = [('k_means_iris_8', KMeans(n_clusters=6))
+              ]
+
+fignum = 1
+titles = ['3 clusters']
+for name, est in estimators:
+    fig = plt.figure(fignum, figsize=(4, 3))
+    ax = Axes3D(fig, rect=[0, 0, .95, 1], elev=48, azim=134)
+    est.fit(X)
+    labels = est.labels_
+
+    ax.scatter(X[:, 3], X[:, 0], X[:, 2],
+               c=labels.astype(np.float), edgecolor='r')
+
+    ax.w_xaxis.set_ticklabels([])
+    ax.w_yaxis.set_ticklabels([])
+    ax.w_zaxis.set_ticklabels([])
+    ax.set_xlabel('Viva')
+    ax.set_ylabel('Documentation')
+    ax.set_zlabel('Execution')
+    ax.set_title(titles[fignum - 1])
+    ax.dist = 8
+    fignum = fignum + 1
+
+# Plot the ground truth
+fig = plt.figure(fignum, figsize=(4, 3))
+ax = Axes3D(fig, rect=[0, 0, .95, 1], elev=48, azim=134)
+
+for name, label in [('Low Grade', 0),
+                    ('Average Grade', 1),
+                    ('Good Grade ', 2)]:
+    ax.text3D(X[y == label, 3].mean(),
+              X[y == label, 0].mean(),
+              X[y == label, 2].mean() + 2, name,
+              horizontalalignment='center',
+              bbox=dict(alpha=.2, edgecolor='g', facecolor='w'))
+# Reorder the labels to have colors matching the cluster results
+y = np.choose(y, [1, 2, 0]).astype(np.float)
+ax.scatter(X[:, 3], X[:, 0], X[:, 2], c=y, edgecolor='r')
+
+ax.w_xaxis.set_ticklabels([])
+ax.w_yaxis.set_ticklabels([])
+ax.w_zaxis.set_ticklabels([])
+ax.set_xlabel('Viva')
+ax.set_ylabel('Documentation')
+ax.set_zlabel('Execution')
+ax.set_title('AI tutor')
+ax.dist = 8
+
+fig.show()
